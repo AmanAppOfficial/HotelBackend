bd2d46a6a53659e9d93cbe48d64c2dff87209a63
Srivastava <aman.d.srivastava@accenture.com>
2021-08-31 20:17:15 +0530

    eureka server

---
+++ src/test/java/com/example/eserver/ContactServcie1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactServcie1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
